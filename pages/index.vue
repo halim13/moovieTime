cbfdce1077c7773c22550cb81efc1ef19efe48c0
halim13 <hasanudinhalim@gmail.com>
2024-12-17 12:37:55 +0700

    meta

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
+<script setup lang="ts">
+import {useHead} from 'nuxt/app'
+useHead({
+  title: 'MoovieTime',
+  meta: [
+    {
+      name: 'description',
+      content: 'movie database',
+    },
+    {
+      name: 'keywords',
+      content: 'moovie, list movie, download movie',
+    },
+  ]
+})
+</script>
+
 <template>
   <Carousel />
-
-  <!-- discover -->
   <Discover />
-  <!-- end discover -->
 </template>
